f35e02c481f47ae8214a3c19f4f453e3f00ed522
ervinanovlianti <ervinanov03@gmail.com>
2023-07-23 12:14:42 +0800

    update keluhancontroller

--- app/Http/Controllers/KeluhanController.php
+++ app/Http/Controllers/KeluhanController.php
@@ -13,37 +13,21 @@ class KeluhanController extends Controller
     {
         $data_keluhan = DB::table('data_keluhan')
             ->join('data_kategori', 'data_keluhan.kategori_id', '=', 'data_kategori.id_kategori')
-            ->select('data_keluhan.*', 'data_kategori.kategori_keluhan')
+            ->join('data_pengguna_jasa', 'data_keluhan.id_pengguna', '=', 'data_pengguna_jasa.id_pengguna')
+            ->select('data_keluhan.*', 'data_kategori.kategori_keluhan', 'data_pengguna_jasa.nama')
+            ->orderBy('tgl_keluhan', 'desc')
             ->get();
 
         return view('data_keluhan', compact('data_keluhan'));
     }
+    public function dataPenggunaJasa()
+    {
+        $data_penggunajasa = DB::table('data_pengguna_jasa')->get();
+
+        return view('data_penggunajasa', compact('data_penggunajasa'));
+    }
     public function showInputForm()
     {
         return view('input_keluhan');
-    }
-    function store(Request $request) {
-        $dataKeluhan = KeluhanModel::create([
-            'tgl_keluhan' => $request->input('tgl_keluhan'),
-            'id_pengguna'    => $request->input('id_pengguna'),
-            'via_keluhan'     => $request->input('via_keluhan'),
-            'uraian_keluhan'   => $request->input('uraian_keluhan'),
-            // 'jenis_costumer'  => $request->input('jenis_costumer'),
-            // 'nama_perusahaan' => $request->input('nama_perusahaan'),
-            // 'nomor_telepon'   => $request->input('nomor_telepon'),
-        ]);
-        // simpan gambar jika ada
-        // if ($request->hasFile('file_gambar')) {
-        //     $file = $request->file('file_gambar');
-            
-        //     // simpan gambar ke dalam storage dengan nama unik
-        //     $path = $file->store('public/gambar');
-
-        //     // simpan path gambar ke dalam database
-        //     $dataKeluhan->file_gambar= $path;
-        //     $dataKeluhan->save();
-        // }
-
-        return redirect('keluhan')->with('success', 'Data keluhan berhasil disimpan!');
     }
 }
