36acc5265d3f7a0baa57c86f37071ae09a4c9375
ervinanovlianti <ervinanov03@gmail.com>
2023-07-23 22:03:02 +0800

    update detail keluhan

--- resources/views/detail_keluhan.blade.php
+++ resources/views/detail_keluhan.blade.php
@@ -1,102 +1,84 @@
 @extends('layouts.main')
 @section('content')
- <div class="row">
-   <div class="row justify-content-center">
-            <div class="col-12">
-              <h2 class="page-title text-center">Detail Keluhan (Nama Customer)</h2>
-              <div class="row">
-                <div class="col-md-6 my-2">
-                  <div class="card shadow">
-                    <div class="card-body">
-                      <h5 class="card-title">Bordered table</h5>
-                      {{-- <p class="card-text">Add .table-bordered for borders on all sides of the table and cells.</p> --}}
-                      <div class="col-md-6 my-4">
-                        <p>Nama Pelapor     : Er</p>
-                        <p>Costumer         : Er</p>
-                        <p>Tanggal Laporan  : Er</p>
-                        <p>Via Laporan      : Er</p>
-                        <p>Gambar           : <a href="" class="btn btn-sm btn-primary">Lihat Gambar</a></p> 
-                      </div>
-                    </div> <!-- end section -->
-                  </div>
-                </div> <!-- end section -->
-                <div class="col-md-6 my-2">
-                  <div class="card shadow">
-                    <div class="card-body">
-                      <h5 class="card-title">Simple Table</h5>
-                      {{-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> --}}
-                      <p>Uraian Keluhan     : Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eaque amet at! Ad ut rem officiis provident neque a, dolor cum modi magni quae ea quam architecto ullam impedit tempore! Amet temporibus aliquam molestiae facere enim eos excepturi suscipit sed, unde, assumenda magni dolores quam?</p>
-                      <p>Kategori         : Pembayaran</p>
-                      <p>Status         : Baru</p>
-                      <p>Aksi</p>
-                      <a href="" class="btn btn-secondary m-auto">Kembali</a>
-                      <a href="" class="btn btn-danger m-auto">Tolak</a>
-                      <a href="" class="btn btn-success m-auto">Verifikasi</a>
-                    </div>
-                  </div>
-                </div> <!-- simple table -->
-              </div>
-              <div class="row">
-                <div class="col-md-6 my-2">
-                  <div class="card shadow">
-                    <div class="card-body">
-                      <h5 class="card-title">Bordered table</h5>
-                      {{-- <p class="card-text">Add .table-bordered for borders on all sides of the table and cells.</p> --}}
-                      <div class="col-md-6 my-4">
-                        <p>Nama Pelapor     : Er</p>
-                        <p>Costumer         : Er</p>
-                        <p>Tanggal Laporan  : Er</p>
-                        <p>Via Laporan      : Er</p>
-                        <p>Gambar           : <a href="" class="btn btn-sm btn-primary">Lihat Gambar</a></p> 
-                      </div>
-                    </div> <!-- end section -->
-                  </div>
-                </div> <!-- end section -->
-                <div class="col-md-6 my-2">
-                  <div class="card shadow">
-                    <div class="card-body">
-                      <h5 class="card-title">Simple Table</h5>
-                      {{-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> --}}
-                      <p>Uraian Keluhan     : Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eaque amet at! Ad ut rem officiis provident neque a, dolor cum modi magni quae ea quam architecto ullam impedit tempore! Amet temporibus aliquam molestiae facere enim eos excepturi suscipit sed, unde, assumenda magni dolores quam?</p>
-                      <p>Kategori         : Pembayaran</p>
-                      <p>Status         : Baru</p>
-                      <p>Aksi</p>
-                      <a href="" class="btn btn-secondary m-auto">Kembali</a>
-                      <a href="" class="btn btn-success m-auto">Terima</a>
-                    </div>
-                  </div>
-                </div> <!-- simple table -->
-              </div>
-              <div class="row">
-                <div class="col-md-6 my-2">
-                  <div class="card shadow">
-                    <div class="card-body">
-                      <h5 class="card-title">Bordered table</h5>
-                      {{-- <p class="card-text">Add .table-bordered for borders on all sides of the table and cells.</p> --}}
-                      <div class="col-md-6 my-4">
-                        <p>Nama Pelapor     : Er</p>
-                        <p>Costumer         : Er</p>
-                        <p>Tanggal Laporan  : Er</p>
-                        <p>Via Laporan      : Er</p>
-                        <p>Gambar           : <a href="" class="btn btn-sm btn-primary">Lihat Gambar</a></p> 
-                      </div>
-                    </div> <!-- end section -->
-                  </div>
-                </div> <!-- end section -->
-                <div class="col-md-6 my-2">
-                  <div class="card shadow">
-                    <div class="card-body">
-                      <h5 class="card-title">Simple Table</h5>
-                      {{-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> --}}
-                      <p>Uraian Keluhan     : Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eaque amet at! Ad ut rem officiis provident neque a, dolor cum modi magni quae ea quam architecto ullam impedit tempore! Amet temporibus aliquam molestiae facere enim eos excepturi suscipit sed, unde, assumenda magni dolores quam?</p>
-                      <p>Kategori         : Pembayaran</p>
-                      <p>Status         : Baru</p>
-                      <p>Aksi</p>
-                      <a href="" class="btn btn-secondary m-auto">Kembali</a>
-                      <a href="" class="btn btn-success m-auto">Konfirmasi Selesai</a>
-                    </div>
-                  </div>
-                </div> <!-- simple table -->
-              </div>
+<div class="row">
+  <div class="row justify-content-center">
+    <div class="col-12">
+      <h3 class="page-title">Detail Keluhan {{ $keluhan->id_keluhan }}</h3>
+      <div class="row">
+        <div class="col-6 my-2">
+          <div class="card shadow">
+            <div class="card-body">
+                <p>Nama Pelapor     : {{ $keluhan->nama }}</p>
+                <p>Costumer         : {{ $keluhan->nama }}</p>
+                <p>Tanggal Laporan  : {{ $keluhan->tgl_keluhan }}</p>
+                <p>Via Laporan      : {{ $keluhan->via_keluhan }}</p>
+                <p>Gambar           : <a href="" class="btn btn-sm btn-primary">Lihat Gambar</a></p> 
             </div>
+          </div>
+        </div>
+        <div class="col-6 my-2">
+          <div class="card shadow">
+            <div class="card-body">
+              <p>Uraian Keluhan : <strong>{{ $keluhan->uraian_keluhan }}</strong> </p>
+              <p>Kategori       : {{ $keluhan->kategori_keluhan }}</p>
+              <p>Status         : {{ $keluhan->status_keluhan }}</p>
+              <p>Aksi           : {{ $keluhan->aksi }}</p>
+              <a href="/keluhan" class="btn btn-secondary m-auto">Kembali</a>
+              {{-- <a href="" class="btn btn-danger m-auto">Tolak</a> --}}
+              <a href="/verifikasi-keluhan/{{ $keluhan->id_keluhan }}" class="btn btn-success m-auto">Verifikasi</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 my-2">
+          <div class="card shadow">
+            <div class="card-body">
+                <p>Nama Pelapor     : {{ $keluhan->nama }}</p>
+                <p>Costumer         : {{ $keluhan->nama }}</p>
+                <p>Tanggal Laporan  : {{ $keluhan->tgl_keluhan }}</p>
+                <p>Via Laporan      : {{ $keluhan->via_keluhan }}</p>
+                <p>Gambar           : <a href="" class="btn btn-sm btn-primary">Lihat Gambar</a></p> 
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="card shadow">
+            <div class="card-body">
+              <p>Uraian Keluhan : <strong>{{ $keluhan->uraian_keluhan }}</strong> </p>
+              <p>Kategori       : {{ $keluhan->kategori_keluhan }}</p>
+              <p>Status         : {{ $keluhan->status_keluhan }}</p>
+              <p>Aksi           : {{ $keluhan->aksi }}</p>
+              <a href="" class="btn btn-secondary m-auto">Kembali</a>
+              <a href="/terima-keluhan/{{ $keluhan->id_keluhan }}" class="btn btn-success m-auto">Terima</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 my-2">
+          <div class="card shadow">
+            <div class="card-body">
+              <p>Nama Pelapor    : {{ $keluhan->nama }}</p>
+              <p>Costumer        : {{ $keluhan->nama }}</p>
+              <p>Tanggal Laporan : {{ $keluhan->tgl_keluhan }}</p>
+              <p>Via Laporan     : {{ $keluhan->via_keluhan }}</p>
+              <p>Gambar          : <a href="" class="btn btn-sm btn-primary">Lihat Gambar</a></p> 
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="card shadow">
+            <div class="card-body">
+              <p>Uraian Keluhan :  {{ $keluhan->nama }}</p>
+              <p>Kategori       :  {{ $keluhan->kategori_keluhan }}</p>
+              <p>Status         :  {{ $keluhan->status_keluhan }}</p>
+              <p>Aksi           :  {{ $keluhan->aksi }}</p>
+              <a href="" class="btn btn-secondary m-auto">Kembali</a>
+              <a href="/keluhan-selesai/{{ $keluhan->id_keluhan }}" class="btn btn-success m-auto">Konfirmasi Selesai</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 @endsection
