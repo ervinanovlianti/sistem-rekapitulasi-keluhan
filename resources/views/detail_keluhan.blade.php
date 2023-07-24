11dfa23a443ce3afbf6088ed604a2a2a297d853c
ervinanovlianti <ervinanov03@gmail.com>
2023-07-24 18:20:17 +0800

    update halaman detai-keluhan

--- resources/views/detail_keluhan.blade.php
+++ resources/views/detail_keluhan.blade.php
@@ -4,7 +4,8 @@
   <div class="row justify-content-center">
     <div class="col-12">
       <h3 class="page-title">Detail Keluhan {{ $keluhan->id_keluhan }}</h3>
-      <div class="row">
+      @if ( $keluhan->status_keluhan == 'menunggu verifikasi')
+        <div class="row">
         <div class="col-6 my-2">
           <div class="card shadow">
             <div class="card-body">
@@ -30,7 +31,8 @@
           </div>
         </div>
       </div>
-      <div class="row">
+      @elseif ( $keluhan->status_keluhan == 'dialihkan ke cs')
+          <div class="row">
         <div class="col-md-6 my-2">
           <div class="card shadow">
             <div class="card-body">
@@ -55,6 +57,7 @@
           </div>
         </div>
       </div>
+      @elseif ( $keluhan->status_keluhan == 'ditangani oleh cs')
       <div class="row">
         <div class="col-md-6 my-2">
           <div class="card shadow">
@@ -80,5 +83,31 @@
           </div>
         </div>
       </div>
+      @else
+      <div class="row">
+        <div class="col-md-6 my-2">
+          <div class="card shadow">
+            <div class="card-body">
+              <p>Nama Pelapor    : {{ $keluhan->nama }}</p>
+              <p>Costumer        : {{ $keluhan->nama }}</p>
+              <p>Tanggal Laporan : {{ $keluhan->tgl_keluhan }}</p>
+              <p>Via Laporan     : {{ $keluhan->via_keluhan }}</p>
+              <p>Gambar          : <a href="" class="btn btn-sm btn-primary">Lihat Gambar</a></p> 
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="card shadow">
+            <div class="card-body">
+              <p>Uraian Keluhan :  {{ $keluhan->nama }}</p>
+              <p>Kategori       :  {{ $keluhan->kategori_keluhan }}</p>
+              <p>Status         :  {{ $keluhan->status_keluhan }}</p>
+              <p>Aksi           :  {{ $keluhan->aksi }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      @endif
+      
     </div>
 @endsection
