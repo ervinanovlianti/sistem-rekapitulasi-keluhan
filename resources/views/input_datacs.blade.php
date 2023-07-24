162998faf18b6d2b3c671a896e8560f5a50a8445
ervinanovlianti <ervinanov03@gmail.com>
2023-07-24 18:20:43 +0800

    update fitur tambah data cs

--- resources/views/input_datacs.blade.php
+++ resources/views/input_datacs.blade.php
@@ -4,33 +4,30 @@
     <div class="col-md-6">
         <div class="card shadow mb-4">
         <div class="card-body">
-            <form>
+            <form method="post" action="/input-datacs">
+                @csrf
                 <div class="mb-3">
                     <label for="" class="form-label">Nama</label>
-                    <input type="text" class="form-control" id="" name="nama_cs">
+                    <input type="text" class="form-control" id="" name="nama">
                 </div>
                 <div class="mb-3">
-                    <label for="" class="form-label">No. Telepon</label>
-                    <input type="text" class="form-control" id="">
+                    <label for="" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="" name="username">
                 </div>
                 <div class="mb-3">
-                    <label for="" class="form-label">E-mail</label>
-                    <input type="text" class="form-control" id="">
+                    <label for="" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="" name="email">
                 </div>
                 <div class="mb-3">
                     <label for="" class="form-label">Password</label>
-                    <input type="text" class="form-control" id="">
+                    <input type="password" class="form-control" id="" name="password">
                 </div>
-                <div class="form-group mb-3">
-                    <label for="example-select">Departemen</label>
-                    <select class="form-control" id="example-select">
-                        <option>--Pilih--</option>
-                        <option>IL</option>
-                        <option>ILCS</option>
-                    </select>
+                <div class="mb-3">
+                    <label for="" class="form-label">No. Telepon</label>
+                    <input type="text" class="form-control" id="" name="no_telepon">
                 </div>
-                <a href="/" class="btn btn-secondary">Kembali</a>
-                <a href="/" class="btn btn-primary">Tambah</a>
+                <a href="/dashboard" class="btn btn-secondary">Kembali</a>
+                <button type="submit" class="btn btn-primary">Tambah</button>
             </form>
         </div> <!-- /.card-body -->
     </div> <!-- /.card -->
