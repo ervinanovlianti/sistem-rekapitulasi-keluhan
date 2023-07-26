b8c629167d85c5352419b1fbf4b072b796ad2abf
ervinanovlianti <ervinanov03@gmail.com>
2023-07-26 22:50:39 +0800

    update halaman view rekapitulasi

--- resources/views/rekapitulasi.blade.php
+++ resources/views/rekapitulasi.blade.php
@@ -6,8 +6,10 @@
         <h2 class="h4 mb-1 text-center">Rekapitulasi Keluhan Pengguna Jasa Berdasarkan Kategori</h2>
         <div class="card shadow">
         <div class="card-body">
-        <!-- table -->
-            {{-- <table class="table table-hover table-borderless border-v ">
+            <!-- keluhan.rekapitulasi.blade.php -->
+
+        
+            <table class="table table-hover table-borderless border-v ">
                 <thead class="thead-dark">
                     <tr class="text-center">
                         <th rowspan="2">No</th>
@@ -18,8 +20,8 @@
                     </tr>
                     <tr role="row">
                             <th>Visit</th>
-                            <th>Web</th>
                             <th>HP/Wa</th>
+                            <th>Web</th>
                             <th>TW</th>
                             <th>Selesai</th>
                             <th>Belum Selesai</th>
@@ -33,61 +35,48 @@
                         <td>Pembayaran</td>
                         <td>{{ $jumlahPembayaran }}</td>
                         <td>{{ $jumlahPembayaran1 }}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>{{ $no++ }}</td>
-                        <td>Penerimaan</td>
-                        <td>{{ $jumlahPengiriman }}</td>
-                        <td>{{ $jumlahPengiriman1 }}</td>
-                       
+                        <td>{{ $jumlahPembayaran2 }}</td>
+                        <td>{{ $jumlahPembayaran3 }}</td>
+                        <td>{{ $statusKeluhanPembayaran1 }}</td>
+                        <td>{{ $statusKeluhanPembayaran2 }}</td>
+                        <td>{{ $statusKeluhanPembayaran3 }}</td>
                     </tr>
                     <tr>
                         <td>{{ $no++ }}</td>
                         <td>Pengiriman</td>
-                        
+                        <td>{{ $jumlahPengiriman }}</td>
+                        <td>{{ $jumlahPengiriman1 }}</td>
+                        <td>{{ $jumlahPengiriman2 }}</td>
+                        <td>{{ $jumlahPengiriman3 }}</td>
+                    </tr>
+                    <tr>
+                        <td>{{ $no++ }}</td>
+                        <td>Penerimaan</td>
                         <td>{{ $jumlahPenerimaan }}</td>
                         <td>{{ $jumlahPenerimaan1 }}</td>
+                        <td>{{ $jumlahPenerimaan2 }}</td>
+                        <td>{{ $jumlahPenerimaan3 }}</td>
                     </tr>
                     <tr>
                         <td>{{ $no++ }}</td>
                         <td>Administrasi</td>
                         <td>{{ $jumlahAdministrasi }}</td>
                         <td>{{ $jumlahAdministrasi1 }}</td>
-                        
+                        <td>{{ $jumlahAdministrasi2 }}</td>
+                        <td>{{ $jumlahAdministrasi3 }}</td>
                     </tr>
                     <tr>
                         <td>{{ $no++ }}</td>
                         <td>Lainnya</td>
                         <td>{{ $jumlahLainnya }}</td>
                         <td>{{ $jumlahLainnya1 }}</td>
+                        <td>{{ $jumlahLainnya2 }}</td>
+                        <td>{{ $jumlahLainnya3 }}</td>
                     </tr>
                 </tbody>
-            </table> --}}
-
-
-            <h1>Hasil Perhitungan Jumlah Kategori Keluhan</h1>
-
-    <table>
-        <tr>
-            <th>Kategori Keluhan</th>
-            <th>Jumlah</th>
-        </tr>
-        @foreach ($jumlahKategoriKeluhan as $data)
-        <tr>
-            {{-- <td>{{ $data->kategori_id }}</td> --}}
-            <td>{{ $data->jumlah_kategori }}</td>
-        </tr>
-        @endforeach
-        @foreach ($jumlahKategoriKeluhan1 as $data1)
-        <tr>
-            <td>{{ $data1->kategori_id }}</td>
-            <td>{{ $data1->jumlah_kategori }}</td>
-        </tr>
-        @endforeach
-    </table>
+            </table> 
         </div>
         </div>
-    </div> <!-- simple table -->
-    </div> <!-- end section -->
+    </div>
+    </div>
 @endsection
