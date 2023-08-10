cf4e44fe698069b4dc2d47270a3769ab0dd00508
ervinanovlianti <ervinanov03@gmail.com>
2023-08-10 11:02:47 +0800

    finishing part 2

--- resources/views/detail_keluhan.blade.php
+++ resources/views/detail_keluhan.blade.php
@@ -14,151 +14,130 @@
         {{ Session::get('error') }}
       </div>
       @endif
-      @if ( Auth::user()->hak_akses == 'admin' && $keluhan->status_keluhan == 'menunggu verifikasi')
       <div class="row">
         <div class="col-6 my-2">
           <div class="card shadow">
             <div class="card-body">
-              <p>Nama Pelapor : {{ $keluhan->nama }}</p>
-              <p>Costumer : {{ $keluhan->jenis_pengguna }}</p>
-              <p>Tanggal Laporan : {{ date('d/m/Y H:i:s', strtotime($keluhan->tgl_keluhan)) }}</p>
-              <p>Via Laporan : {{ $keluhan->via_keluhan }}</p>
-              @if ($keluhan->gambar)
-              <p>Gambar : <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#gambarModal{{ $keluhan->id }}">Lihat</button>
-              </p>
-              @endif
+              <table class="table table-hover">
+                <tbody>
+                  <tr>
+                    <th>Nama Pelapor</th>
+                    <td>{{ $keluhan->nama }}</td>
+                  </tr>
+                  <tr>
+                    <th>Customer</th>
+                    <td>{{ $keluhan->jenis_pengguna }}</td>
+                  </tr>
+                  <tr>
+                    <th>Tanggal Laporan</th>
+                    <td>{{ $keluhan->tgl_keluhan }}</td>
+                  </tr>
+                  <tr>
+                    <th>Via Laporan</th>
+                    <td>{{ $keluhan->via_keluhan }}</td>
+                  </tr>
+                  @if ($keluhan->gambar)
+                  <tr>
+                    <th>Gambar</th>
+                    <td>
+                      <button type="button" class="btn btn-sm btn-primary" data-toggle="modal"
+                        data-target="#gambarModal{{ $keluhan->id }}">Lihat</button>
+                    </td>
+                  </tr>
+                  @endif
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
         <div class="col-6 my-2">
           <div class="card shadow">
             <div class="card-body">
-              <p>Uraian Keluhan : <strong>{{ $keluhan->uraian_keluhan }}</strong> </p>
-              <p>Kategori : {{ $keluhan->kategori_keluhan }}</p>
-              <p>Status Keluhan : {{ $keluhan->status_keluhan }}</p>
-              {{-- <p>Aksi : {{ $keluhan->aksi }}</p> --}}
-              <a href="/keluhan" class="btn btn-secondary m-auto">Kembali</a>
-              {{-- <a href="" class="btn btn-danger m-auto">Tolak</a> --}}
-              <button type="button" class="btn btn-success m-auto" data-toggle="modal" data-target="#verifikasiModal" data-whatever="@mdo">Verifikasi</button>
+              <table class="table table-hover">
+                <tbody>
+                  <tr>
+                    <th>Uraian Keluhan</th>
+                    <td>{{ $keluhan->uraian_keluhan }}</td>
+                  </tr>
+                  <tr>
+                    <th>Kategori</th>
+                    <td>{{ $keluhan->kategori_keluhan }}</td>
+                  </tr>
+                  <tr>
+                    <th>Status Keluhan</th>
+                    <td>{{ $keluhan->status_keluhan }}</td>
+                  </tr>
+                  @if ($keluhan->waktu_penyelesaian)
+                  <tr>
+                    <th>Waktu Penyelesaian</th>
+                    <td>{{ $keluhan->waktu_penyelesaian }}</td>
+                  </tr>
+                  @endif
+                  @if ($keluhan->aksi)
+                  <tr>
+                    <th>Aksi</th>
+                    <td>{{ $keluhan->aksi }}</td>
+                  </tr>
+                  @endif
+                  <tr>
+                    @if ( Auth::user()->hak_akses == 'admin' && $keluhan->status_keluhan == 'menunggu verifikasi')
+                    <th>Action</th>
+                    <td>
+                      <a href="/keluhan" class="btn btn-secondary m-auto">Kembali</a>
+                      {{-- <a href="" class="btn btn-danger m-auto">Tolak</a> --}}
+                      <button type="button" class="btn btn-success m-auto" data-toggle="modal"
+                        data-target="#verifikasiModal" data-whatever="@mdo">Verifikasi</button>
+                    </td>
+                    @elseif ( Auth::user()->hak_akses == 'customer_service' && $keluhan->status_keluhan == 'dialihkan ke
+                    cs')
+                    <th>Action</th>
+                    <td>
+                      <a href="/keluhan" class="btn btn-secondary m-auto">Kembali</a>
+                      <a href="/terima-keluhan/{{ $keluhan->id_keluhan }}" class="btn btn-success m-auto">Terima</a>
+                    </td>
+                    @elseif ( Auth::user()->hak_akses == 'customer_service' && $keluhan->status_keluhan == 'ditangani
+                    oleh cs')
+                    <th>Action</th>
+                    <td>
+                      {{-- <a href="/keluhan" class="btn btn-secondary m-auto">Kembali</a> --}}
+                      <button type="button" class="btn btn-success m-auto" data-toggle="modal"
+                        data-target="#konfirmasiModal" data-whatever="@mdo">Konfirmasi</button>
+                    </td>
+                    @endif
+                  </tr>
+                </tbody>
+              </table>
+
             </div>
           </div>
         </div>
       </div>
-      @elseif ( Auth::user()->hak_akses == 'customer_service' && $keluhan->status_keluhan == 'dialihkan ke cs')
-      <div class="row">
-        <div class="col-md-6 my-2">
-          <div class="card shadow">
-            <div class="card-body">
-              <p>Nama Pelapor : {{ $keluhan->nama }}</p>
-              <p>Costumer : {{ $keluhan->jenis_pengguna }}</p>
-              <p>Tanggal Laporan : {{ $keluhan->tgl_keluhan }}</p>
-              <p>Via Laporan : {{ $keluhan->via_keluhan }}</p>
-              @if ($keluhan->gambar)
-              <p>Gambar : <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#gambarModal{{ $keluhan->id }}">Lihat</button>
-              </p>
-              @endif
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="card shadow">
-            <div class="card-body">
-              <p>Uraian Keluhan : <strong>{{ $keluhan->uraian_keluhan }}</strong> </p>
-              <p>Kategori : {{ $keluhan->kategori_keluhan }}</p>
-              <p>Status : {{ $keluhan->status_keluhan }}</p>
-              <a href="/keluhan" class="btn btn-secondary m-auto">Kembali</a>
-              <a href="/terima-keluhan/{{ $keluhan->id_keluhan }}" class="btn btn-success m-auto">Terima</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      @elseif ( Auth::user()->hak_akses == 'customer_service' && $keluhan->status_keluhan == 'ditangani oleh cs')
-      <div class="row">
-        <div class="col-md-6 my-2">
-          <div class="card shadow">
-            <div class="card-body">
-              <p>Nama Pelapor : {{ $keluhan->nama }}</p>
-              <p>Costumer : {{ $keluhan->jenis_pengguna }}</p>
-              <p>Tanggal Laporan : {{ $keluhan->tgl_keluhan }}</p>
-              <p>Via Laporan : {{ $keluhan->via_keluhan }}</p>
-              @if ($keluhan->gambar)
-              <p>Gambar : <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#gambarModal{{ $keluhan->id }}">Lihat</button>
-              </p>
-              @endif
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="card shadow">
-            <div class="card-body">
-              <p>Uraian Keluhan : <strong>{{ $keluhan->uraian_keluhan }}</strong></p>
-              <p>Kategori : {{ $keluhan->kategori_keluhan }}</p>
-              <p>Penangungjawab : {{ $namaCS->nama }}</p>
-              <p>Status : {{ $keluhan->status_keluhan }}</p>
-              {{-- <p>Aksi : {{ $keluhan->aksi }}</p> --}}
-              <a href="/keluhan" class="btn btn-secondary m-auto">Kembali</a>
-              <button type="button" class="btn btn-success m-auto" data-toggle="modal" data-target="#konfirmasiModal" data-whatever="@mdo">Konfirmasi</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      @elseif(Auth::user()->hak_akses == 'admin' && $keluhan->status_keluhan == 'ditangani oleh cs' || 'dialihkan ke cs'
-      || 'selesai')
-      <div class="row">
-        <div class="col-md-6 my-2">
-          <div class="card shadow">
-            <div class="card-body">
-              <p>Nama Pelapor : {{ $keluhan->nama }}</p>
-              <p>Costumer : {{ $keluhan->jenis_pengguna }}</p>
-              <p>Tanggal Laporan : {{ $keluhan->tgl_keluhan }}</p>
-              <p>Via Laporan : {{ $keluhan->via_keluhan }}</p>
-              @if ($keluhan->gambar)
-              <p>Gambar : <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#gambarModal{{ $keluhan->id }}">Lihat</button>
-              </p>
-              @endif
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="card shadow">
-            <div class="card-body">
-              <p>Uraian Keluhan : <strong> {{ $keluhan->uraian_keluhan }}</strong></p>
-              <p>Kategori : {{ $keluhan->kategori_keluhan }}</p>
-              <p>Penangungjawab : {{ $namaCS->nama }}</p>
-              <p>Status : {{ $keluhan->status_keluhan }}</p>
-              @if (!empty($keluhan->waktu_penyelesaian))
-              <p>Waktu Penyelesaian : {{ $keluhan->waktu_penyelesaian }}</p>
-              @endif
-              @if (!empty($keluhan->aksi))
-                <p>Aksi : {{ $keluhan->aksi }}</p>
-              @endif
-            </div>
-          </div>
-        </div>
-      </div>
-      @endif
+
     </div>
 
     {{-- Modal Lihat Gambar --}}
-    <div class="modal fade" id="gambarModal{{ $keluhan->id }}" tabindex="-1" role="dialog" aria-labelledby="gambarModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="gambarModalLabel">Gambar Keluhan</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                @if ($keluhan->gambar)
-                  <img src="{{ asset('gambar_keluhan/' . $keluhan->gambar) }}" alt="Gambar Keluhan" style="max-width: 100%; height: auto;">
-                @endif
-                </div>
-            </div>
+    <div class="modal fade" id="gambarModal{{ $keluhan->id }}" tabindex="-1" role="dialog"
+      aria-labelledby="gambarModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="gambarModalLabel">Gambar Keluhan</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            @if ($keluhan->gambar)
+            <img src="{{ asset('gambar_keluhan/' . $keluhan->gambar) }}" alt="Gambar Keluhan"
+              style="max-width: 100%; height: auto;">
+            @endif
+          </div>
         </div>
+      </div>
     </div>
     {{-- Modal Verifkasi --}}
-    <div class="modal fade" id="verifikasiModal" tabindex="-1" role="dialog" aria-labelledby="verifikasiModalLabel" aria-hidden="true">
+    <div class="modal fade" id="verifikasiModal" tabindex="-1" role="dialog" aria-labelledby="verifikasiModalLabel"
+      aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -190,7 +169,8 @@
         </div>
       </div>
     </div>
-    <div class="modal fade" id="konfirmasiModal" tabindex="-1" role="dialog" aria-labelledby="konfirmasiModalLabel" aria-hidden="true">
+    <div class="modal fade" id="konfirmasiModal" tabindex="-1" role="dialog" aria-labelledby="konfirmasiModalLabel"
+      aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
